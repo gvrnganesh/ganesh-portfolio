1ea70375a680037da131e7fcda38190048ca5ae3
gvrnganesh <gvrnganesh@gmail.com>
2025-03-20 19:05:48 +0530

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,12 @@ function About() {
     <section id="about" className="about">
       <h2>About Me</h2>
       <p>
-        Hi, I'm <strong>Ganesh</strong>, 👋 Hi, I'm Ganesh, a passionate Frontend Developer with a strong foundation in the MERN stack. I love building responsive, user-friendly, and dynamic web applications that enhance user experiences.
+      👋 Hi, I'm <strong>Ganesh</strong>, a passionate Frontend Developer with a strong foundation in the MERN stack. I love building responsive, user-friendly, and dynamic web applications that enhance user experiences.
+      <br/>
 💡 Skilled in HTML, CSS, JavaScript, React.js, Node.js, Express.js, MongoDB, Python, SQL, Git, and GitHub, I continuously explore new technologies to stay ahead in the industry.
+<br/>
 🚀 Currently, I'm sharpening my expertise in full-stack development, focusing on performance optimization and scalable applications.
+<br/>
 📩 Open to frontend development roles, internships, and freelance opportunities. Let’s connect and create something amazing!
       </p>
     </section>
